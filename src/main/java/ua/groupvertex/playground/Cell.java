64349042e07a4c7e990a7cb6cc4b8904b6f5cd4a
Rostislav <yeaxi98@gmail.com>
2016-07-10 14:14:58 +0300

    playground were added

--- src/main/java/ua/groupvertex/playground/Cell.java
+++ src/main/java/ua/groupvertex/playground/Cell.java
@@ -5,19 +5,19 @@ public class Cell {  // save as Cell.java
    public Seed content; // content of this cell of type Seed.
                  // take a value of Seed.EMPTY, Seed.CROSS, or Seed.NOUGHT
    int row, col; // row and column of this cell, not used in this program
- 
+
    /** Constructor to initialize this cell */
    public Cell(int row, int col) {
       this.row = row;
       this.col = col;
       clear();  // clear content
    }
- 
+
    /** Clear the cell content to EMPTY */
    public void clear() {
       content = Seed.EMPTY;
    }
- 
+
    /** Paint itself */
    public void paint() {
       switch (content) {
@@ -25,5 +25,15 @@ public class Cell {  // save as Cell.java
          case NOUGHT: System.out.print(" O "); break;
          case EMPTY:  System.out.print("   "); break;
       }
+   }
+
+   public String str() {
+      String str  = "";
+      switch (content) {
+         case CROSS:  str=(" X "); break;
+         case NOUGHT: str=(" O "); break;
+         case EMPTY:  str=("   "); break;
+      }
+       return str;
    }
 }
